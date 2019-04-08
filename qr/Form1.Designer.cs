3181aad11e5601eb3aea4f17ce5ca1c9ab681d96
pkdgood <sean4787@gmail.com>
2019-04-08 09:53:35 +0900

    Upload Project

---
+++ qr/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace qr
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(854, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 110);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "barcode maker";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(57, 59);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(385, 223);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(547, 261);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(216, 21);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(854, 177);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(133, 105);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "barcode reader";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(545, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(232, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "setted URL => https://www.google.com";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(57, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "barcode maker result";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(545, 236);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(126, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "barcode reader result";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(59, 373);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(610, 12);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "1. Press barcode maker button. QRcode will generated on your desktop path by sett" +
+    "ed URL in source code";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(59, 405);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(604, 12);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "2. Press barcode reader button. Generated QRcode when \'barcode maker\' pressed wil" +
+    "l be convert in URL";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1121, 530);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ qr/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using BusinessRefinery.Barcode;
+using System.Drawing.Imaging;
+
+namespace qr
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        /*{
+            QRCode barcode = new QRCode();
+            string url = "http://www.naver.com";    // QR코드에 저장할 내용
+            barcode.Code = url;
+            barcode.ModuleSize = 6.0f;
+            barcode.Resolution = 300;
+            barcode.drawBarcode2ImageFile("naver.png");  // 프로젝트 폴더의 Debug 경로내에 파일이 생성됩니다.
+
+            MessageBox.Show("done.");
+        }*/
+        {
+            ZXing.BarcodeWriter barcodeWriter = new ZXing.BarcodeWriter();
+            barcodeWriter.Format = ZXing.BarcodeFormat.QR_CODE;
+
+            barcodeWriter.Options.Width = this.pictureBox1.Width;
+            barcodeWriter.Options.Height = this.pictureBox1.Height;
+
+            string strQRCode = "http://www.google.com";
+
+            this.pictureBox1.Image = barcodeWriter.Write(strQRCode);
+            string deskPath = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+            barcodeWriter.Write(strQRCode).Save(deskPath + @"\test.jpg", ImageFormat.Jpeg);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ZXing.BarcodeReader barcodeReader = new ZXing.BarcodeReader();
+            string deskPath = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+
+            System.IO.FileInfo fi = new System.IO.FileInfo(deskPath + @"\test.jpg");
+
+            if (fi.Exists)
+            {
+                var barcodeBitmap = (Bitmap)Image.FromFile(deskPath + @"\test.jpg");
+                var result = barcodeReader.Decode(barcodeBitmap);
+
+                this.textBox1.Text = result.Text;
+            }
+            else
+            {
+                MessageBox.Show("Please generate QRcode first.\n Press barcode maker button.", "Nofile", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
